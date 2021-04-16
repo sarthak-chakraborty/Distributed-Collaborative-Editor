a603f8e14a96e3bb9c8e337b79fc62e42648ecf3
nnjha98 <nnjha98@gmail.com>
2021-04-16 23:21:54 +0530

    Add files via upload

--- master/editor/urls.py
+++ master/editor/urls.py
@@ -4,9 +4,7 @@ from editor import views
 urlpatterns = [
 	url(r'^$', views.index, name='index-default'),
 	url(r'^api/HB/$',views.heartbeat_recv, name='heart-beat'),
-	url(r'^api/users/$', views.users),
-	url(r'^api/users/(?P<user_id>[^/]+)/$', views.user),
-	url(r'^api/documents/(?P<document_id>[^/]+)/$', views.document),
-	url(r'^api/documents/(?P<document_id>[^/]+)/changes/$', views.document_changes, name='document-changes'),
-	url(r'^(?P<document_id>[^/]+)$', views.index)
+	url(r'^api/documents/(?P<document_id>[^./]+)/$', views.document),
+	url(r'^api/documents/(?P<document_id>[^./]+)/changes/$', views.document_changes, name='document-changes'),
+	url(r'^(?P<document_id>[^./]+)$', views.index)
 ]
