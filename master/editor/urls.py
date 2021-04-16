4f8e7ce1c688bff47319af8ea2b7b3cdd447ce7c
nnjha98 <nnjha98@gmail.com>
2021-04-16 13:31:28 +0530

    Add files via upload

--- master/editor/urls.py
+++ master/editor/urls.py
@@ -4,11 +4,12 @@ from editor import views
 urlpatterns = [
 	url(r'^$', views.index, name='index-default'),
 	url(r'^api/HB/$',views.heartbeat_recv, name='heart-beat'),
+	url(r'^api/lease_new/$',views.lease_new, name='lease_new'),
+	url(r'^api/lease_extend/$',views.lease_extend, name='lease_extend'),
+	url(r'^api/lease_extend_ack/$',views.lease_extend_ack, name='lease_extend_ack'),
 	url(r'^api/users/$', views.users),
 	url(r'^api/users/(?P<user_id>[^/]+)/$', views.user),
 	url(r'^api/documents/(?P<document_id>[^/]+)/$', views.document),
 	url(r'^api/documents/(?P<document_id>[^/]+)/changes/$', views.document_changes, name='document-changes'),
-	url(r'^(?P<document_id>[^/]+)$', views.index),
-	url(r'^api/become_primary/$', views.become_primary, name='become_primary'),
-	url(r'^api/become_secondary/$',views.become_secondary, name='become_secon')
+	url(r'^(?P<document_id>[^/]+)$', views.index)
 ]
